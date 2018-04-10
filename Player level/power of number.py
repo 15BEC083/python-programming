da0685e358cbbae2ceb3e4f782a1d022b96bf03c
15BEC083 <37336257+15BEC083@users.noreply.github.com>
2018-04-10 18:42:10 +0530

    Create power of number.py

---
+++ Player level/power of number.py
@@ -0,0 +1,21 @@
+M=[]
+N,K=raw_input().split(' ')
+n=int(N)
+k=int(K)
+for i in range(1,n+1):
+	b=k**i
+	M.append(b)
+if N in M:
+	print("yes")
+else:
+	print("no")M=[]
+N,K=raw_input().split(' ')
+n=int(N)
+k=int(K)
+for i in range(1,n+1):
+	b=k**i
+	M.append(b)
+if N in M:
+	print("yes")
+else:
+	print("no")
