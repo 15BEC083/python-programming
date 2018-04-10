ce2c2d2a91c13fe670876200fbe47723d570f1d9
15BEC083 <37336257+15BEC083@users.noreply.github.com>
2018-04-10 18:43:08 +0530

    Update power of number.py

--- Player level/power of number.py
+++ Player level/power of number.py
@@ -8,14 +8,4 @@ for i in range(1,n+1):
 if N in M:
 	print("yes")
 else:
-	print("no")M=[]
-N,K=raw_input().split(' ')
-n=int(N)
-k=int(K)
-for i in range(1,n+1):
-	b=k**i
-	M.append(b)
-if N in M:
-	print("yes")
-else:
 	print("no")
